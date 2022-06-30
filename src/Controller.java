ad5256ad26c3c2645c6adafa6a6594fc92fff1eb
Jun Wei <32480707+junwei00@users.noreply.github.com>
2022-06-30 17:55:57 +0800

    Feat: save text to txt file

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Controller implements Initializable{
@@ -24,5 +30,23 @@ public class Controller implements Initializable{
   public void save() {
     System.out.println("Saved: ");
     System.out.println(textArea.getText());
+
+    FileChooser fileChooser = new FileChooser();
+
+    try {
+      fileChooser.setTitle("Save As");
+      File file = fileChooser.showSaveDialog(stage);
+
+      if (file != null) {
+        FileWriter fw = new FileWriter(file);
+        BufferedWriter out = new BufferedWriter(fw);
+        out.write(textArea.getText());
+        out.close();
+      }
+    } catch (FileNotFoundException e) {
+      
+    } catch (IOException e) {
+
+    }
   }
 }
